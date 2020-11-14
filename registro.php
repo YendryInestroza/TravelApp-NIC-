b0e0b3d80ecc4d7fb9d4d452366c1c8864273df2
Yendry Inestroza <michy_yinestroza30@hotmail.com>
2020-11-14 02:00:10 -0600

    Version 1

---
+++ registro.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TravelApp Nic</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0 maximum-scale=1.0, minium-scale=1.0" type="text/css">
+
+</head>
+
+<body class="bg-login">
+
+    <div class="container p-3">
+        <div class="row">
+            <div class="col-md-5">
+
+            </div>
+            <div class="col-md-7 shadow-lg my-5 bg-light p-3">
+                <h3>Crear Cuenta </h3>
+                <form action="" class="row mt-3">
+                    <div class="form-group col-md-6">
+                        <label for="" >Nombre</label>
+                        <input type="text" name="" id="" class="form-control">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="" >Apellido</label>
+                        <input type="text" name="" id="" class="form-control">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="" >Fecha de Nacimiento</label>
+                        <input type="date" name="" id="" class="form-control">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="" >Nacionalidad </label>
+                        <select class="form-control">
+                        <option>Honduras</option>
+                        <option>Nicaragua</option>
+                        <option>Estados Unidos</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="" >Correo</label>
+                        <input type="e-mail" name="" id="" class="form-control">
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="" >Contraseña</label>
+                        <input type="password" name="" id="" class="form-control">
+                    </div>
+                </form>
+                    <div class="text-center">
+                            <button class="btn btn-primary">Registrar</button><hr>
+                            <p>Ya tienes una cuenta igresa aquí</p>
+                            <a href="login.php">Iniciar Sesión</a>
+                    </div>
+                
+            </div>
+             
+            </div>
+        </div>
+    
+
+
+
+<script src="js/jquery-3.4.1.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
---
+++ reservacion.php
@@ -0,0 +1,45 @@
+<?php include "header.php"?>
+
+        <div class="container">
+            <h3 class="linea mt-5"  id="color-n"><span>RESERVACIÓN</span></h3>
+
+            <form action="" class="row p-5">
+            <div class="form-group col-md-4">
+                    <label for="" >Fecha de Entrada</label>
+                    <input type="date" name="" id="" class="form-control">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="" >fecha de Salida</label>
+                    <input type="date" name="" id="" class="form-control">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="" >Procedencia</label>
+                    <input type="" name="" id="" class="form-control">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="" >Nombre</label>
+                    <input type="text" name="" id="" class="form-control">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="" >Apellido</label>
+                    <input type="text" name="" id="" class="form-control">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="" >No. de Identificación</label>
+                    <input type="text" name="" id="" class="form-control">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="" >No. de Teléfono</label>
+                    <input type="text" name="" id="" class="form-control">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="" >Correo</label>
+                    <input type="email" name="" id="" class="form-control">
+                </div>
+            </form>
+                <div class="text-center">
+                    <button class="btn btn-info">Reservar</button>
+                </div>
+        </div>
+
+<?php include "footer.php"?>
